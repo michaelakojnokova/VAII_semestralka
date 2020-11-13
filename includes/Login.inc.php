2fbb5680f26d98fcf9874801deb11daf256f9d02
Michaela Kojnokova <kojnokova1@stud.uniza.sk>
2020-11-13 17:17:33 +0100

    PHP_version4 uz funguje registracia aj kontrola formularovych prvkov aj validacia

--- includes/Login.inc.php
+++ includes/Login.inc.php
@@ -1,1 +1,34 @@
 <?php
+$serverName = "localhost";
+$dBUsername = "root";
+$dBPassword    = "";
+$dBName = "vaii_database";
+$conn = mysqli_connect($serverName,$dBUsername,$dBPassword ,$dBName);
+
+if (isset($_POST["submit"])) {
+    $username = $_POST["uid"];
+    $password = $_POST["psw"];
+
+    require_once 'dbh.inc.php';
+    require_once 'functions.inc.php';
+
+    if (emptyInputLogin( $username, $password) !== false) { //ak je prazdny , tak error
+        header("location: ../Login.php?error=emptyInput"); //toto sa ukaze v url ak niekto zabudol dat nieco do form
+        exit();
+    }
+/*
+    if (invalidUid($username) !== false) { //ak nie je validne username, tak error
+        header("location: ../Signup.php?error=invalidUid"); //toto sa ukaze v url
+        exit();
+    }
+    if (pwdMatch($password) !== false) { //
+        header("location: ../Signup.php?error=passwordsDontMatch"); //toto sa ukaze v url
+        exit();
+    }
+    */
+    loginUser($conn, $username,$password);
+}
+else {
+    header("location: ../Login.php");
+    exit();
+}
