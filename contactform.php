fea6c5d8ed65020d55a0ab910c42c0eaf2dd3ab9
Michaela Kojnokova <kojnokova1@stud.uniza.sk>
2020-12-27 14:06:40 +0100

    PHP_version10- NEJDE POSLAT MAIL

---
+++ contactform.php
@@ -0,0 +1,23 @@
+<?php
+
+if (isset($_POST['submit'])) {
+
+    $name = $_POST['name'];
+    $subject = $_POST['subject'];
+    $mailFrom = $_POST['mail'];
+    $message = $_POST['message'];
+
+    $mailTo = "miskakojnokova@gmail.com";
+    $headers = "From: " . $mailFrom;
+    $txt = "You have received an e-mail from" . $name . ". \n\n" . $message;
+
+    mail($mailTo, $subject, $txt, $headers);
+
+    header("Location: AboutMe.php?mailsent" );
+    echo "Email successfully sent  ..";
+
+}
+else {
+    echo "Email sending failed...";
+}
+
