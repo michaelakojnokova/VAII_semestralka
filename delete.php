cfa72cf77dab167382c900206b9ac472c97e0036
Michaela Kojnokova <kojnokova1@stud.uniza.sk>
2021-01-16 21:26:25 +0100

    PHP_version13 - zoznam uzivatelov funguje TO DO : dorob delete, uprav tabulku dizajnovo, sprav profil, dopln registraciu o firstName,lastName,age

--- delete.php
+++
@@ -1,8 +0,0 @@
-<?php
-$connect = mysqli_connect("localhost", "root", "", "vaii_database");
-if (isset($_POST["usersId"])) {
-    $query = "DELETE FROM users WHERE usersId = '".$_POST["usersId"]."'";
-    if (mysqli_query($connect, $query)) {
-        echo 'Data Deleted';
-    }
-}
